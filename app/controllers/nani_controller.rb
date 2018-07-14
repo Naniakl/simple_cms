86f18cbd33a4d9878100c11003ca1a8394b09c67
nani <you@example.com>
2018-07-14 19:59:36 +0530

    create render and redirect_to

--- app/controllers/nani_controller.rb
+++ app/controllers/nani_controller.rb
@@ -1,12 +1,21 @@
 class NaniController < ApplicationController
   def index
   	#render(:template => 'nani/hello')
-    render('nani/hello')
+    #render('nani/hello')
+    render('hello')
   end
 
   def hello
    	#render(:template => 'nani/index')
-        render('nani/index')
+    #render('nani/index')
+     render('index')
+  end
+   
+  def other_hello
+  	redirect_to(:action => 'hello')
   end
 
+  def facebook
+    redirect_to('https://www.facebook.com')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get 'nani/index'
   get 'nani/hello'
+  get 'nani/other_hello'
+  get 'nani/facebook'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
