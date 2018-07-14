38cb9bc80df322fc63391e0cfb6d4af816ff529a
nani <you@example.com>
2018-07-14 19:20:18 +0530

    render template with both short hand and long hand versions

--- app/controllers/nani_controller.rb
+++ app/controllers/nani_controller.rb
@@ -1,4 +1,12 @@
 class NaniController < ApplicationController
   def index
+  	#render(:template => 'nani/hello')
+    render('nani/hello')
   end
+
+  def hello
+   	#render(:template => 'nani/index')
+        render('nani/index')
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+ 
+  root 'nani#index'
+
   get 'nani/index'
+  get 'nani/hello'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
